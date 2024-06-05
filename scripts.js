6779c980045a4a3d906201ceee4a190fa5840fe1
Pratyoos Panta <pratyoos.panta@outlook.com>
2024-06-05 16:58:37 +0545

    created array values

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
-let programmingDivsInputsArrayValues = Array.from(document.getElementById('programming-language').getElementsByTagName('input')).map(input => input.id);
-console.log(programmingDivsInputsArrayValues);
+let programmingLanguageValues = Array.from(document.getElementById('programming-language').getElementsByTagName('input')).map(input => input.id);
+console.log(programmingLanguageValues);
 
+let frontEndValues = Array.from(document.getElementById('frontend').getElementsByTagName('input')).map(input => input.id);
+console.log(frontEndValues);
+
+let backEndValues = Array.from(document.getElementById('backend').getElementsByTagName('input')).map(input => input.id);
+console.log(backEndValues);
+
+let mobileAppValues = Array.from(document.getElementById('mobile-app').getElementsByTagName('input')).map(input => input.id);
+console.log(mobileAppValues);
+
+let databaseValues = Array.from(document.getElementById('database').getElementsByTagName('input')).map(input => input.id);
+console.log(databaseValues);
+
+let aimlValues = Array.from(document.getElementById('ai-ml').getElementsByTagName('input')).map(input => input.id);
+console.log(aimlValues);
+
+let dataVisualizationValues = Array.from(document.getElementById('data-visualization').getElementsByTagName('input')).map(input => input.id);
+console.log(dataVisualizationValues);
+
+console.log(programmingLanguageValues.length);
