40882ae259b08e7926c56426faa1c4f156efe742
Pratyoos Panta <pratyoos.panta@outlook.com>
2024-06-13 12:39:04 +0545

    updated files

--- scripts.js
+++ scripts.js
@@ -18,3 +18,22 @@ console.log(aimlValues);
 
 let dataVisualizationValues = Array.from(document.getElementById('data-visualization').getElementsByTagName('input')).map(input => input.id);
 console.log(dataVisualizationValues);
+
+let form = document.querySelector('form');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let titleContent = document.querySelector(".title").querySelector('span').textContent + " " + document.querySelector(".title").querySelector('input').value;
+    console.log(titleContent);
+
+    let subtitleContent = document.querySelector(".subtitle").querySelector('input').value;
+    console.log(subtitleContent);
+
+    let workContent = [];
+    document.querySelector(".work").querySelectorAll('.input-group').forEach(inputGrp => {
+        let temp = [];
+        temp.push(inputGrp.querySelector('span').textContent);
+        temp.push(inputGrp.querySelector('input').value);
+        workContent.push(temp);
+    });
+    console.log(workContent)
+});
